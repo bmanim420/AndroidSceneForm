4e0459829f85d0140dac33be64a698a13f6275d0
bmanim420 <bm.anim420@gmail.com>
2019-07-28 15:23:05 +0600

    second commit

--- app/src/main/java/com/example/androidsceneform/MainActivity.java
+++ app/src/main/java/com/example/androidsceneform/MainActivity.java
@@ -128,34 +128,42 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         };
     }
 
+
     @Override
     public void onClick(View v) {
-        if(v.getId() == R.id.bear)
-        {
-            selected = 1;
-            setBackground(v.getId());
-        }
-        else if(v.getId() == R.id.cat)
-        {
-            selected = 2;
-            setBackground(v.getId());
-        }
-        else if(v.getId() == R.id.cow)
-        {
-            selected = 3;
-            setBackground(v.getId());
-        }
+
     }
 
-    private void setBackground(int id) {
-        for (int i=0;i<arrayView.length;i++)
-        {
-            if (arrayView[i].getId() == id)
-                arrayView[i].setBackgroundColor(Color.parseColor("#80333639"));
-            else
-                arrayView[i].setBackgroundColor(Color.TRANSPARENT);
-        }
-    }
+
+//
+//    @Override
+//    public void onClick(View v) {
+//        if(v.getId() == R.id.bear)
+//        {
+//            selected = 1;
+//            setBackground(v.getId());
+//        }
+//        else if(v.getId() == R.id.cat)
+//        {
+//            selected = 2;
+//            setBackground(v.getId());
+//        }
+//        else if(v.getId() == R.id.cow)
+//        {
+//            selected = 3;
+//            setBackground(v.getId());
+//        }
+//    }
+//
+//    private void setBackground(int id) {
+//        for (int i=0;i<arrayView.length;i++)
+//        {
+//            if (arrayView[i].getId() == id)
+//                arrayView[i].setBackgroundColor(Color.parseColor("#80333639"));
+//            else
+//                arrayView[i].setBackgroundColor(Color.TRANSPARENT);
+//        }
+//    }
 
 
 }
